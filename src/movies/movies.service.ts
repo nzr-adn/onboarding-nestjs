ffae87161153d192a3a957734a517cee2ecde635
LAPTOP-GQT1FNKP\NZR <nizar.adian.n@gmail.com>
2023-03-11 22:54:24 +0700

    testing logging

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { MoviesEntity } from '../model/movies.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,6 +9,8 @@ export class MoviesService {
   @InjectRepository(MoviesEntity)
   private readonly moviesRepository: Repository<MoviesEntity>;
 
+  private readonly logger = new Logger();
+
   test(): string {
     return 'Movies Service Work !';
   }
@@ -18,6 +20,7 @@ export class MoviesService {
     movie.title = payload.title;
     movie.overview = payload.overview;
     movie.poster = payload.poster;
+    this.logger.log('Insert Movie');
     return this.moviesRepository.save(movie);
   }
 }
